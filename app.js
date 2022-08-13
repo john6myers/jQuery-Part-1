a00a5861e321cc3a97bf315b5d0ce9859f00d162
john6myers <john6myers@gmail.com>
2022-08-13 16:38:49 -0400

    part 1

---
+++ app.js
@@ -0,0 +1,35 @@
+// 1
+$(document).ready(function(){
+    console.log( '“Let’s get ready to party with jQuery!”')
+});
+
+// 2
+$('article img').addClass('image-center');
+
+// 3
+$('article p:last-child').remove();
+
+// 4
+$('#title').css('font-size', Math.random() * 100)
+
+// 5
+$("#list").append('<li>I love my doggo ollie</li>')
+
+// 6
+$('aside')
+    .empty()
+    .append($('<p>', {text: "Sorry about that list"}));
+
+// 7
+$(".form-control").on('keyup blur change', function () {
+    let red = $(".form-control").eq(0).val();
+    let blue = $(".form-control").eq(1).val();
+    let green = $(".form-control").eq(2).val();
+    $("body").css("background-color",
+        "rgb(" + red + "," + green + "," + blue + ")");
+  });
+
+// 8
+$('img').on('click', function(){
+    $('img').remove()
+})
